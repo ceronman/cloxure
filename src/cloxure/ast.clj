f4021d8cd7d4d37bcc01b8cec09c36b46a26da4c
Manuel Cerón <ceronman@gmail.com>
2020-04-16 19:01:23 +0200

    Added a printer for reverse polish notation

--- src/cloxure/ast.clj
+++ src/cloxure/ast.clj
@@ -14,7 +14,9 @@
 (defn- group [expression]
   {:type :group :expression expression})
 
-(defmulti pretty-print :type)
+(defmulti pretty-print
+  "Converts an AST to string using a lisp-like notatation" 
+  :type)
 
 (defn- parenthesize [name & expressions]
   (str "(" name " " (str/join " " (map pretty-print expressions)) ")"))
@@ -25,15 +27,39 @@
 (defmethod pretty-print :unary [{op :operator r :right}]
   (parenthesize (:text op) r))
 
-(defmethod pretty-print :group [{op :operator e :expression}]
+(defmethod pretty-print :group [{e :expression}]
   (parenthesize "group" e))
 
 (defmethod pretty-print :literal [{value :value}]
   (if (nil? value) "nil" (str value)))
 
-(def sample (binary (unary (scanner/token :minus "-")
-                           (literal 123))
-                    (scanner/token :star "*")
-                    (group (literal 45.67))))
+(def sample )
 
-(comment (pretty-print sample))
+(comment (pretty-print (binary (unary (scanner/token :minus "-")
+                                      (literal 123))
+                               (scanner/token :star "*")
+                               (group (literal 45.67)))))
+
+(defmulti rpn-print
+  "Converts and AST to string using Reverse Polish Notation"
+  :type)
+
+(defmethod rpn-print :binary [{op :operator l :left r :right}]
+  (str/join " " [(rpn-print l) (rpn-print r) (:text op)]))
+
+(defmethod rpn-print :unary [{op :operator r :right}]
+  (str/join " " [(rpn-print r) (:text op)]))
+
+(defmethod rpn-print :group [{e :expression}]
+  (rpn-print e))
+
+(defmethod rpn-print :literal [{value :value}]
+  (if (nil? value) "nil" (str value)))
+
+(comment (rpn-print (binary (binary (literal 1)
+                                    (scanner/token :plus "+")
+                                    (literal 2))
+                            (scanner/token :star "*")
+                            (binary (literal 4)
+                                    (scanner/token :minus "-")
+                                    (literal 3)))))
