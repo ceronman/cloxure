5080a4fb7e863ea0c4b809164004445f8e521515
Manuel Cerón <ceronman@gmail.com>
2020-05-22 10:16:05 +0200

    Some bug fixes to the resolver and support for if-statements

--- src/cloxure/resolver.clj
+++ src/cloxure/resolver.clj
@@ -12,9 +12,8 @@
 (defn- add-var [resolver name-token ready?]
   (let [[scope & others] (:scopes resolver)]
     (if scope
-      (update resolver
-              :scopes
-              conj others (assoc scope (:text name-token) ready?))
+      (let [new-scope (assoc scope (:text name-token) ready?)]
+        (assoc resolver :scopes (conj others new-scope)))
       resolver)))
 
 (defn- add-local [resolver node pos]
@@ -25,37 +24,48 @@
          scopes (:scopes resolver)]
     (cond
       (empty? scopes) resolver
-      (contains? (peek scopes) (:text name-token)) (add-local resolver node pos)
+()      (contains? (peek scopes) (:text name-token)) (add-local resolver node pos)
       :else (recur (inc pos) (rest scopes)))))
 
-(defmulti static-resolve
+(defmulti resolve-locals
   "Statically resolves scoping of variables"
-  (fn [node _] (:type node)))
+  (fn [_ node] (:type node)))
 
 (defn resolve-statements [resolver statements]
-  (reduce static-resolve resolver statements))
+  (reduce resolve-locals resolver statements))
 
-(defmethod static-resolve :block [resolver node]
+(defmethod resolve-locals :block [resolver node]
   (let [resolver (update resolver :scopes conj {})
         resolver (resolve-statements resolver (:statements node))]
     (update resolver :scopes pop)))
 
-(defmethod static-resolve :var-stmt [resolver node]
-  (let [name-token(:name-token node)
+(defmethod resolve-locals :var-stmt [resolver node]
+  (let [name-token (:name-token node)
         resolver (add-var resolver name-token false)
         initializer (:initializer node)
-        resolver (if initializer (static-resolve initializer resolver) resolver)]
+        resolver (if initializer (resolve-locals resolver initializer) resolver)]
     (add-var resolver name-token true)))
 
-(defmethod static-resolve :variable [resolver node]
+(defmethod resolve-locals :variable [resolver node]
   (let [name-token (:name-token node)
         [scope & _] (:scopes resolver)
         resolver (if (and scope (false? (get scope (:text name-token))))
-                   (error resolver 
-                          name-token 
+                   (error resolver
+                          name-token
                           "Cannot read local variable in its own initializer.")
                    resolver)]
     (resolve-local resolver node name-token)))
+
+(defmethod resolve-locals :assign [resolver node]
+  (let [resolver (resolve-locals resolver (:value-expr node))]
+    (resolve-local resolver node (:name-token node))))
+
+(defmethod resolve-locals :if-stmt [resolver node]
+  (let [resolver (resolve-locals resolver (:condition node))
+        resolver (resolve-locals resolver (:then-branch node))]
+    (if-let [else-branch (:else-branch node)]
+      (resolve-locals resolver else-branch)
+      resolver)))
 
 (require '[cloxure.scanner :as scanner])
 (require '[cloxure.parser :as parser])
@@ -67,7 +77,10 @@
       (let [{errors :errors statements :statements} (parser/parse tokens)]
         (if (seq errors)
           errors
-          (:locals (resolve-statements (new-resolver) statements)))))))
+          (let [{errors :errors locals :locals} (resolve-statements (new-resolver) statements)]
+            (if (seq errors)
+              errors
+              locals)))))))
 
 (comment (test-resolver
           "var a;"))
@@ -77,3 +90,12 @@
 
 (comment (test-resolver
           "{ var a; { a; } }"))
+
+(comment (test-resolver
+          "{ var a; { var a = a; } }"))
+
+(comment (test-resolver
+          "{ var a; var b; a = b; }"))
+
+(comment (test-resolver
+          "{ var a; if (a) { a = b; } }"))
