e260f389dde3325f883a9c0c3750f35e3a802ff7
Manuel Cerón <ceronman@gmail.com>
2020-07-05 15:59:14 +0200

    Small refactorings to resolver

--- src/cloxure/resolver.clj
+++ src/cloxure/resolver.clj
@@ -5,39 +5,44 @@
    [cloxure.error :refer [token-error]]))
 
 (defn- new-resolver []
-  {::scopes '() ; TODO: replace list with vector?
+  {::scopes '()
    ::locals {}
    ::errors []
    ::current-fn nil
    ::current-class nil})
 
-(defn- error [resolver token message]
+(defn- add-error [resolver token message]
   (update resolver ::errors conj (token-error token message)))
 
-(defn- add-var [resolver name-token ready?]
-  (let [[scope & others] (::scopes resolver)
+(defn- add-to-scope [resolver name ready?]
+  (let [[head & tail] (::scopes resolver)]
+    (assoc resolver ::scopes (conj tail (assoc head name ready?)))))
+
+(defn- declare-name [resolver name-token ready?]
+  (let [scope (peek (::scopes resolver))
         name (::token/lexeme name-token)]
     (if scope
       (if (and (not ready?) (contains? scope name))
-        (error resolver name-token 
+        (add-error resolver name-token 
                "Variable with this name already declared in this scope.")
-        (let [new-scope (assoc scope (::token/lexeme name-token) ready?)]
-          (assoc resolver ::scopes (conj others new-scope))))
+        (add-to-scope resolver name ready?))
       resolver)))
 
-(defn- add-vars [resolver name-tokens ready?]
-  (reduce #(add-var %1 %2 ready?) resolver name-tokens))
-
-(defn- add-local [resolver node pos]
-  (update resolver ::locals assoc node pos))
+(defn- declare-names [resolver name-tokens ready?]
+  (reduce #(declare-name %1 %2 ready?) resolver name-tokens))
 
 (defn- resolve-local [resolver node name-token]
   (loop [pos 0
          scopes (::scopes resolver)]
     (cond
-      (empty? scopes) resolver
-      (contains? (peek scopes) (::token/lexeme name-token)) (add-local resolver node pos)
-      :else (recur (inc pos) (rest scopes)))))
+      (empty? scopes) 
+      resolver
+      
+      (contains? (peek scopes) (::token/lexeme name-token)) 
+      (update resolver ::locals assoc node pos)
+      
+      :else 
+      (recur (inc pos) (rest scopes)))))
 
 (defn- begin-scope [resolver]
   (update resolver ::scopes conj {}))
@@ -60,16 +65,16 @@
 
 (defmethod resolve-locals ::ast/var-stmt [resolver node]
   (let [name-token (::ast/name-token node)
-        resolver (add-var resolver name-token false)
+        resolver (declare-name resolver name-token false)
         initializer (::ast/initializer node)
         resolver (if initializer (resolve-locals resolver initializer) resolver)]
-    (add-var resolver name-token true)))
+    (declare-name resolver name-token true)))
 
 (defmethod resolve-locals ::ast/variable [resolver node]
   (let [name-token (::ast/name-token node)
         [scope & _] (::scopes resolver)
         resolver (if (and scope (false? (get scope (::token/lexeme name-token))))
-                   (error resolver
+                   (add-error resolver
                           name-token
                           "Cannot read local variable in its own initializer.")
                    resolver)]
@@ -85,15 +90,15 @@
     (-> resolver
         (begin-scope)
         (assoc ::current-fn fn-type)
-        (add-vars (::ast/params fun-stmt) false)
-        (add-vars (::ast/params fun-stmt) true)
+        (declare-names (::ast/params fun-stmt) false)
+        (declare-names (::ast/params fun-stmt) true)
         (resolve-statements (::ast/body fun-stmt))
         (assoc ::current-fn prev-fn)
         (end-scope))))
 
 (defmethod resolve-locals ::ast/fun-stmt [resolver node]
   (-> resolver
-      (add-var (::ast/name-token node) true)
+      (declare-name (::ast/name-token node) true)
       (resolve-function node :function)))
 
 (defn- resolve-methods [resolver methods]
@@ -109,7 +114,7 @@
   (-> resolver
       (resolve-locals superclass)
       (begin-scope)
-      (add-var {::token/lexeme "super"} true)))
+      (add-to-scope "super" true)))
 
 (defmethod resolve-locals ::ast/class-stmt [resolver node]
   (let [prev-class (::current-class resolver)
@@ -117,14 +122,14 @@
         superclass (::ast/superclass node)
         superclass-name (::token/lexeme (::ast/name-token superclass))]
     (if (and superclass (= name superclass-name))
-      (error resolver
+      (add-error resolver
              (::ast/name-token superclass)
              "A class cannot inherit from itself.")
       (-> resolver
-          (add-var (::ast/name-token node) true)
+          (declare-name (::ast/name-token node) true)
           (cond-> superclass (resolve-superclass superclass))
           (begin-scope)
-          (add-var {::token/lexeme "this"} true) ;; TODO: hacky!
+          (add-to-scope "this" true)
           (assoc ::current-class (if (::ast/superclass node) :subclass :class))
           (resolve-methods (::ast/methods node))
           (assoc ::current-class prev-class)
@@ -178,7 +183,7 @@
 
 (defmethod resolve-locals ::ast/this-expr [resolver node]
   (if (nil? (::current-class resolver))
-    (error resolver
+    (add-error resolver
            (::ast/keyword node)
            "Cannot use 'this' outside of a class.")
     (resolve-local resolver node (::ast/keyword node))))
@@ -186,19 +191,19 @@
 (defmethod resolve-locals ::ast/super [resolver node]
   (case (::current-class resolver)
     :subclass (resolve-local resolver node (::ast/keyword node))
-    nil (error resolver (::ast/keyword node) 
+    nil (add-error resolver (::ast/keyword node) 
                "Cannot use 'super' outside of a class.")
-    (error resolver (::ast/keyword node) 
+    (add-error resolver (::ast/keyword node) 
            "Cannot use 'super' in a class with no superclass.")))
 
 (defmethod resolve-locals ::ast/return-stmt [resolver node]
   (if (nil? (::current-fn resolver))
-    (error resolver
+    (add-error resolver
            (::ast/keyword node)
            "Cannot return from top-level code.")
     (if (::ast/value node)
       (if (= (::current-fn resolver) ::ast/initializer)
-        (error resolver
+        (add-error resolver
                (::ast/keyword node)
                "Cannot return a value from an initializer.")
         (resolve-locals resolver (::ast/value node)))
